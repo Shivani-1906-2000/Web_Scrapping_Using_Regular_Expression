2286b1739ea4c108f61a4ebffae1a8c95c25c52d
Shivani-1906-2000 <43836101+Shivani-1906-2000@users.noreply.github.com>
2019-10-04 06:47:43 +0530

    Delete title.py

--- title.py
+++
@@ -1,6 +0,0 @@
-from urllib.request import Request,urlopen
-import re
-req=Request("https://www.google.com",headers={'User-Agent': 'Mozilla/5.0'})
-webpage = urlopen(req).read()
-match=re.findall('<title>.*</title>',str(webpage),re.IGNORECASE)
-print(match[0])
